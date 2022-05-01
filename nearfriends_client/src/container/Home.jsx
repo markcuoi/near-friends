3b8543db3247f32bae0707aa10d933dfc8d15eff
markshiba <thanhphan10397@gmail.com>
2022-05-01 15:03:38 +0700

    update project

---
+++ nearfriends_client/src/container/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="flex flex-col justify-start items-center h-screen">
+      Login
+    </div>
+  );
+};
+
+export default Home;
